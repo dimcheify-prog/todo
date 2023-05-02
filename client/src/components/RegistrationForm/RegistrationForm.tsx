38a167a7c9e656f92889824edd90b3df7bc25565
Дмитрий Перелыгин <dimcheify@Dmitrijs-MacBook-Air.local>
2023-05-03 00:01:31 +0300

    quick fixes

---
+++ client/src/components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,38 @@
+import React, {FC, SyntheticEvent, useContext} from 'react';
+import "./RegisterForm.sass";
+import ErrorHandler from "../ErrorHandler/ErrorHandler";
+import {Link, useNavigate} from "react-router-dom";
+import {useInput} from "../../hooks/useInput";
+import {AppContext} from "../../index";
+import {observer} from "mobx-react-lite";
+
+const RegisterForm: FC = () => {
+    const email = useInput('', {isEmail: false, isEmpty: true});
+    const password = useInput('', {isEmpty: true, minLength: 5});
+
+    const {store} = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const handlerSubmit = (e: SyntheticEvent) => {
+        e.preventDefault();
+        store.auth.registration(email.value, password.value);
+        navigate('/tasks');
+    };
+    return (
+        <form onSubmit={handlerSubmit} className="form-registration">
+            <h2 className="form-registration__title">Регистрация</h2>
+            <input className="form-registration__input" onChange={(e) => email.onChange(e)} value={email.value} onBlur={(e) => email.onBlur(e)} type="email" placeholder="Email"/>
+            {(email.isDirty && email.isEmpty) && <ErrorHandler error={'Пустое поле'}/>}
+            {(email.isDirty && email.emailError) && <ErrorHandler error={'Некорректный email'}/>}
+            <input className="form-registration__input" onChange={(e) => password.onChange(e)} value={password.value} onBlur={(e) => password.onBlur(e)} type="password" placeholder="Password"/>
+            {(password.isDirty && password.isEmpty) && <ErrorHandler error={'Пустое поле'}/>}
+            {(password.isDirty && password.minLengthError) && <ErrorHandler error={'Некоректная длина'}/>}
+            <div className="form-registration__button-box">
+                <button className="form-registration__button" type="submit">Зарегистрироваться</button>
+                <Link className="form-registration__link" to="/login">Уже есть аккаунт?</Link>
+            </div>
+        </form>
+    );
+};
+
+export default observer(RegisterForm);
